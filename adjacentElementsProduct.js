947eeaf44a7a6aa871c45a4aa816abc46b55b862
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-04-23 17:17:29 -0400

    Create adjacentElementsProduct.js

---
+++ adjacentElementsProduct.js
@@ -0,0 +1,7 @@
+function adjacentElementsProduct(inputArray) {
+    let arr = []; 
+    for ( let i = 0; i<inputArray.length-1; i++ ) {
+        arr.push(inputArray[i]*inputArray[i+1])
+    }
+    return Math.max(...arr)
+}
